d7477a041f0f1e218e00ee38436e5e03551b7e57
“[Mojtaba-Pourkhanlar]” <[mojtabapourkhanlar@gmail.com]>
2022-12-06 20:26:04 +0330

    fixed-project

--- src/AppContainer.js
+++ src/AppContainer.js
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useState } from "react";
+import { Box, TextField } from "@mui/material";
+import Hide from "./assets/hide.svg";
+import Show from "./assets/show.svg";
 
 const AppContainer = () => {
-  return (
-    <div>AppContainer</div>
-  )
-}
+  const [show, setShow] = useState(true);
 
-export default AppContainer
+  const togglePassword = () => {
+    setShow(!show);
+  };
+
+  const container = {
+    minHeight: "100vh",
+    backgroundColor: "#ccc",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const showStyle = {
+    position: "absolute",
+    right: 10,
+    top: 15,
+  };
+
+  return (
+    <Box sx={container}>
+      <Box sx={{ position: "relative", width: "350px" }}>
+        <TextField
+          fullWidth
+          variant="outlined"
+          label="Password"
+          type={show ? "password" : "text"}
+        />
+
+        <Box onClick={togglePassword} sx={showStyle}>
+          {show ? (
+            <img src={Show} width="30px" alt="Show" />
+          ) : (
+            <img src={Hide} width="30px" alt="hide" />
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default AppContainer;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppContainer from './AppContainer';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppContainer from "./AppContainer";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppContainer />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
